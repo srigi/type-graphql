0f6617b51b4ea32b6cded6d4175529bbe3cc6857
Srigi <295197+srigi@users.noreply.github.com>
2025-02-14 18:31:08 +0100

    Add API resolver for Movie
    - also put schema.graphql to VCS

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,30 +1,20 @@
-import 'reflect-metadata';
 import { createYoga } from 'graphql-yoga';
 import http from 'node:http';
-import path from 'node:path';
-import { buildSchema } from 'type-graphql';
 
-import { RecipeResolver } from '~/Recipe/resolver';
-import { pubSub } from '~/pubsub';
+import { schemaPromise } from './schema';
 
 const port = +(process.env.PORT || 3000);
 
-async function createServer() {
-  const schema = await buildSchema({
-    emitSchemaFile: path.resolve(__dirname, '../.out/schema.graphql'),
-    pubSub,
-    resolvers: [RecipeResolver],
+schemaPromise
+  .then((schema) => {
+    return http.createServer(
+      createYoga({
+        graphqlEndpoint: '/graphql',
+        healthCheckEndpoint: '/health',
+        schema,
+      }),
+    );
+  })
+  .then((server) => {
+    server.listen(port, '0.0.0.0', () => console.log(`Server is running on port ${port}`));
   });
-
-  const yoga = createYoga({
-    graphqlEndpoint: '/graphql',
-    healthCheckEndpoint: '/health',
-    schema,
-  });
-
-  return http.createServer(yoga);
-}
-
-createServer().then((server) => {
-  server.listen(port, '0.0.0.0', () => console.log(`Server is running on port ${port}`));
-});
