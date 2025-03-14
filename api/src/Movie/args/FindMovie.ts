0f04720eeb5b52891fbc557ebf6e0d50d3c2c139
Srigi <295197+srigi@users.noreply.github.com>
2025-03-14 23:58:12 +0100

    Update API resolver

--- api/src/Movie/args/FindMovie.ts
+++ api/src/Movie/args/FindMovie.ts
@@ -2,6 +2,9 @@ import { ArgsType, Field } from 'type-graphql';
 
 @ArgsType()
 export class FindMovieArgs {
-  @Field((type) => String)
-  publicId!: string;
+  @Field((type) => String, { nullable: true })
+  publicId?: string;
+
+  @Field((type) => String, { nullable: true })
+  slug?: string;
 }
