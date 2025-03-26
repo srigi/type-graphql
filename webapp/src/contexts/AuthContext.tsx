fe4d83790319f3db2cb54cd5429d8ac09ad8b806
Srigi <295197+srigi@users.noreply.github.com>
2025-03-26 23:08:28 +0100

    Fetch user identity on initial render

--- webapp/src/contexts/AuthContext.tsx
+++ webapp/src/contexts/AuthContext.tsx
@@ -1,6 +1,9 @@
-import { h, createContext } from 'preact';
-import { useState } from 'preact/hooks';
+import { createContext } from 'preact';
 import type { JSX } from 'preact';
+import { useState, useEffect } from 'preact/hooks';
+import { useQuery } from 'urql';
+
+import { graphql } from '~gql';
 
 export type AuthUser = {
   publicId: string;
@@ -17,8 +20,24 @@ export const AuthContext = createContext<AuthContextType>({
   setUser: () => {},
 });
 
+const whoamiQuery = graphql(`
+  query Whoami {
+    whoami {
+      publicId
+      username
+    }
+  }
+`);
+
 export const AuthProvider = ({ children }: { children: JSX.Element }) => {
   const [user, setUser] = useState<AuthUser | null>(null);
+  const [{ data, fetching }] = useQuery({ query: whoamiQuery, variables: {} });
+
+  useEffect(() => {
+    if (!fetching && data?.whoami) {
+      setUser(data.whoami);
+    }
+  }, [data, fetching]);
 
   return <AuthContext.Provider value={{ user, setUser }}>{children}</AuthContext.Provider>;
 };
